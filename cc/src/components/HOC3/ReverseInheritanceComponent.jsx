3f729ccace07a5d0e17a90252ebe6ecc519c5fef
kimberly.zou <757872845@qq.com>
2018-08-24 01:12:43 +0800

    cc

--- cc/src/components/HOC3/ReverseInheritanceComponent.jsx
+++ cc/src/components/HOC3/ReverseInheritanceComponent.jsx
@@ -4,10 +4,10 @@ export default function HOC(WrapperComponent){
         test1(){
             return this.test2()+5;
         }
-        // componentDidMount(){
-        //     console.log('1');
-        //     this.setState({number:2});
-        // }
+        componentDidMount(){
+            console.log('1');
+            this.setState({number:2});
+        }
         render(){
             //使用super调用传入组件的render方法
             return super.render();
